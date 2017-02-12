bdf361c10db7f70ff15115454d0f3a5f84d201fa
erdivartanovich <erdivartanovich@gmail.com>
2017-02-12 09:09:48 +0700

    give padding and margin to interaksi detail component

--- src/components/interaksi-detail.js
+++ src/components/interaksi-detail.js
@@ -5,8 +5,10 @@ import {
 } from 'react-native';
 
 const InteraksiDetail = (props) => 
-    <View>
-        <Text> {props.interaksi.name} </Text>
+    <View style={{ paddingLeft: 16 }}>
+        <View style={{ marginTop: 16 }}>
+            <Text style={{ fontSize: 16 }} > {props.interaksi.name} </Text>
+        </View>
     </View>;
 
 export default InteraksiDetail;
